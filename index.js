bcdb85dc3c37d59a4154994719faab752dff78de
mckaymatt3 <mckaymatt@outlook.com>
2022-01-31 16:30:29 -0500

    commit tree a3b10155c028e21c0704e2cbaa3c5df1f832a252

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-
 // Write your code here
+// function multiply(num1,num2) {
+   // return num1*num2;
+// }
+
+// console.log(multiply(62,1));
+
+// function multiply (num1,num2) {
+//    let result = num1 * num2;    
+//        return result
+// }
+
+
+// Test Number One - Multiply
+const num1 = 31
+const num2 = 2
+const multiply = num1* num2
+
+// Test Number Two - Random
+const random = Math.floor(Math.random() * 10) + 1
+
+// Test Number Three - Mod
+const num3 = 40
+const num4 = 9
+const mod = num3 % num4
+
+// Test Number Four - Max
+const max = Math.max(1, 2, 20, 4, 5)
